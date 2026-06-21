69f608e9ef7ce283f21ca81cd4b001a8d779e3b4
Yusen Wang <yusenwang23@gmail.com>
2026-06-21 17:33:37 +0100

    Updated stylesheet path

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
 @app.route('/api/hello')
 def hello():
-    return jsonify({'message': 'Hello from Flask!'})
+ return jsonify({'message': 'Hello from Flask!'})
 if __name__ == '__main__':
-    app.run(debug=True)
+ app.run(debug=True)
